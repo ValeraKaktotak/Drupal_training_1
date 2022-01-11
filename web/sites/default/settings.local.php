28d1f9521f7e65205602f05e36a9b4c274863e10
Valera <valerakaktotak@gmail.com>
2022-01-11 12:30:35 +0200

    fix host name in settings.local.php

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -154,7 +154,6 @@ $settings['skip_permissions_hardening'] = TRUE;
  */
 # $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
 $settings['trusted_host_patterns'] = [
-  '^localhost$',
-  '127\.0\.0\.1',
-  '^valera.backend-education.hulk.nixdev.co/$'
+  '^valera.backend-education.hulk.nixdev.co$',
+  '^.+\.valera.backend-education.hulk.nixdev.co$',
 ];
--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -770,9 +770,9 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
 $databases['default']['default'] = array (
-  'database' => 'valera_drupal',
-  'username' => 'valera_dp',
-  'password' => '11111',
+  'database' => 'drupal_test',
+  'username' => 'root',
+  'password' => '1111',
   'prefix' => '',
   'host' => 'localhost',
   'port' => '3306',
